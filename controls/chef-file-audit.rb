f96c8f9e67c32a3d5493144873c66bfd3518aa49
Jim Quilty <jquilty@chef.io>
2020-07-22 11:54:47 -0400

    Fixed error in InSpec profile syntax
    Signed-off-by: Jim Quilty <jquilty@chef.io>

--- controls/chef-file-audit.rb
+++ controls/chef-file-audit.rb
@@ -8,7 +8,7 @@ control "File-Check-01" do                        # A unique ID for this control
   title "Check for Chef File"                     # A human-readable title
   desc "Checks to see that the Chef file exists and that it's contents matches what is expected."
   describe file('C:\chef.txt') do                 # The actual test
-    it { should_exist }
+    it { should exist }
     its('content') { should eq 'Chef is Great!\r\n' }
   end
 end
